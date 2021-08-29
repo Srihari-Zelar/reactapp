49b5d4d9538448a4449f5ff7ffbfd606443bcab7
Srihari <Srihari@zelarsoft.com>
2021-08-29 21:02:28 +0530

    up

--- api/routes/database.js
+++ api/routes/database.js
@@ -1,18 +1,26 @@
-const { Client } = require ('pg');
+var express = require('express');
+var router = express.Router();
 
-const client = new Client ({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "1234",
-    database: "empdata"
-})
+router.get("/",function(req,res,next){
+    res.send(
+        const { Client } = require ('pg');
 
-client.connect();
+        const client = new Client ({
+            host: "localhost",
+            port: 5432,
+            user: "postgres",
+            password: "1234",
+            database: "empdata"
+        })
 
-client.query(`select * from employees`, (err, result) => {
-        if(!err) {
-                console.log(result.rows);
-        }
-        client.end();
-})
+        client.connect();
+
+        client.query(`select * from employees`, (err, result) => {
+            if(!err) {
+                    console.log(result.rows);
+            }
+            client.end();
+        }))
+
+
+module.exports=router;
