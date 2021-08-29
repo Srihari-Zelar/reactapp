168a64f841eccd42d5e86a1ebbbce89ec0286c9e
Srihari <Srihari@zelarsoft.com>
2021-08-29 21:08:26 +0530

    up

--- api/app.js
+++ api/app.js
@@ -7,7 +7,6 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 testAPIRouter= require("./routes/testAPI");
-databaseRouter= require("./routes/database");
 var app = express();
 
 // view engine setup
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
-app.use("/database", databaseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
