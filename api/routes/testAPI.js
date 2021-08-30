06ea11d50643fdf703d521a4e21a73889cf8a1e9
Srihari <Srihari@zelarsoft.com>
2021-08-30 13:02:12 +0530

    up

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -3,15 +3,6 @@ var router=express.Router();
 
 router.get("/",function(req,res,next){
     res.send("Employees Table");
-    res.send(
-               my-app.query('SELECT * FROM employees', function (err, result) {
-               if (err) {
-                    console.log(err);
-                    res.status(400).send(err);
-               }
-               res.render('employees/list', { title: "Employees", data: result.rows });
-    }))
+});
 
-//});
-
-module.exports=router
+module.exports=router;
