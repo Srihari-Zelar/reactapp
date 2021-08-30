6a60a8de94c0053648ae06a9a963a3493a13506d
Srihari <Srihari@zelarsoft.com>
2021-08-30 15:23:01 +0530

    up

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router=express.Router();
 
 router.get("/",function(req,res,next){
-    res.send(var connect = "postgres://postgres:1234@ocalhost:5432/empdata");
+    res.send( connect = "postgres://postgres:1234@ocalhost:5432/empdata");
 });
 
 module.exports=router;
