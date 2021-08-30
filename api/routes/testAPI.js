b472b97918404cff0b5ed2849bf2343ac45e2d29
Srihari <Srihari@zelarsoft.com>
2021-08-30 09:32:54 +0530

    up

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,20 +1,20 @@
 var express = require("express");
 var router=express.Router();
 
-//router.get("/",function(req,res,next){
-//    res.send("Employees Table");
-//});
-
 router.get("/",function(req,res,next){
-    res.send(
-               my-app.query('SELECT * FROM employees', function (err, result) {
-               if (err) {
-                    console.log(err);
-                    res.status(400).send(err);
-               }
-               res.render('employees/list', { title: "Employees", data: result.rows });
-    }));
-
+    res.send("Employees Table");
 });
+
+//router.get("/",function(req,res,next){
+//    res.send(
+//               my-app.query('SELECT * FROM employees', function (err, result) {
+//               if (err) {
+//                    console.log(err);
+//                    res.status(400).send(err);
+//               }
+//               res.render('employees/list', { title: "Employees", data: result.rows });
+//    }));
+
+//});
 
 module.exports=router;
