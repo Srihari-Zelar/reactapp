2085d8339813e4ebb9c02245128c899d022a3218
Srihari <Srihari@zelarsoft.com>
2021-08-30 14:43:12 +0530

    up

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,4 +5,25 @@ router.get("/",function(req,res,next){
     res.send("Employees Table");
 });
 
+const { Client } = require('pg');
+var connectionString = "postgres://postgres:1234@localhost:5432/empdata";
+
+const client = new Client({
+    connectionString: connectionString
+});
+
+client.connect();
+
+exports.list = function (req, res) {
+
+    client.query('SELECT * FROM employees', function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.render('employees/list', { title: "Employees", data: result.rows });
+    });
+
+};
+
 module.exports=router;
