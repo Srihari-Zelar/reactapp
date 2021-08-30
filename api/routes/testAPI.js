77b609c620bbeba84d3602f30e91e7856a571c31
Srihari <Srihari@zelarsoft.com>
2021-08-30 09:02:33 +0530

    up

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,4 +5,12 @@ router.get("/",function(req,res,next){
     res.send("Employees Table");
 });
 
+app.get("/employees", async (req, res) => {
+    try {
+    const allemployees = await pool.query("SELECT * FROM employees");
+    res.json(allemployees.rows);
+    } catch (err) {
+    console.error(err.message);
+    }
+})
 module.exports=router;
