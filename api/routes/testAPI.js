df8f783e0be7df3951eb0ebd84f2174151e2b275
Srihari <Srihari@zelarsoft.com>
2021-08-30 12:59:32 +0530

    up

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -10,7 +10,7 @@ router.get("/",function(req,res,next){
                     res.status(400).send(err);
                }
                res.render('employees/list', { title: "Employees", data: result.rows });
-    }));
+    }))
 
 //});
 
