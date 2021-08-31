75f39067722b312302a43def9c2e3f3d141ba885
Srihari <Srihari@zelarsoft.com>
2021-08-31 12:32:15 +0530

    up

---
+++ api/api.js
@@ -0,0 +1,20 @@
+const client =require('./database.js')
+const express = require ('express');
+const app = express();
+
+
+app.listen(4000, ()=>{
+    console.log("server is now listening at port 4000");
+})
+
+client.connect();
+
+
+app.get('/emp', (req, res)=>{
+    client.query(`select * from employees`, (err,result) => {
+    if(!err){
+        res.send(result.rows);
+        }
+    });
+    client.end;
+})
