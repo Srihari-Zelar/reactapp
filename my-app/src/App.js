c71e0bda7dc846828f1e884be7c46db0a7fe9f55
Srihari <Srihari@zelarsoft.com>
2021-08-31 13:33:39 +0530

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import employeeslist from './empinfo/employeeslist';
+import employeeslist from './empinfo/Employeeslist';
 
 
 function App(){
